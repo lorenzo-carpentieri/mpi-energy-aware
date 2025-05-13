5e859aa181b5239c343d85ab4e224f0721eb47c3
lorenzo-carpentieri <lorenzocarpentieri34@gmail.com>
2025-05-13 10:24:52 +0200

    Added device and host energy profiling in the code

--- cuda/ar_nccl.cu
+++ cuda/ar_nccl.cu
@@ -18,11 +18,14 @@
 int main(int argc, char *argv[]) {
     int rank, size;
     std::string log_path;
-    if (argc != 2)
-        return -1;
-    else
-        log_path = argv[1];
+    std::string csv_path;
 
+    if (argc != 3)
+        return -1;
+    else{   
+        log_path = argv[1];
+        csv_path = argv[2];
+    }
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
@@ -70,18 +73,21 @@ int main(int argc, char *argv[]) {
         cudaMemcpy(d_recvbuf, h_recvbuf, buff_size_byte[0], cudaMemcpyHostToDevice);
     }
 
-    if (rank == 0)
-        std::cout << "approach,run,chain_size,num_byte,mem_cpy_time_ms,time_ms,min_goodput_Gbs" << std::endl;
-    
+    std::ofstream csv_file(csv_path, std::ios::app);  // use std::ios::app to append if the file exists
+    if (rank == 0){
+        csv_file << "approach,run,chain_size,num_byte,mem_cpy_time_ms,time_ms,min_goodput_Gbs,device_energy,host_energy" << std::endl;
+    }
+
     cudaMemset(d_sendbuf, rank, buff_size_byte[num_iters-1]);
     auto mem_cpy_t_start = std::chrono::high_resolution_clock::now();
     cudaMemcpy(h_sendbuf, d_sendbuf, buff_size_byte[num_iters-1], cudaMemcpyDeviceToHost);
     auto mem_cpy_t_end = std::chrono::high_resolution_clock::now();
 
     for (int i = 0; i < num_iters; i++) {
-        float avg_time_s = 0;
-        int chain_size = 0;
-
+        float avg_time_s = 0; // average execution time for on MAX_RUN for a collective 
+        int chain_size = 0; //  num of times that a collective is executed 
+        double avg_dev_energy_mj = 0;  // average device energy consumption
+        double avg_host_energy_mj = 0; // average host energy consumption
         for (int run = 0; run < MAX_RUN; run++) {
             float ar_time = 0;
             chain_size = 0;
@@ -103,9 +109,17 @@ int main(int argc, char *argv[]) {
                 chain_size++;
             }
             //TODO: print in the csv also the device energy and the host energy. Without the need of parsing power file.
-            powerProf.stop();
+            double dev_energy_mj = powerProf.stop() / static_cast<double>(chain_size); // mJ for a one collective run
+            double host_energy_mj = powerProf.get_host_energy()/ static_cast<double>(chain_size); //host energy in mj for one collective run
+            // Consider the energy consumption consumed by all CPUs and all GPUs of each rank
+            MPI_Allreduce(MPI_IN_PLACE, &host_energy_mj, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+            MPI_Allreduce(MPI_IN_PLACE, &dev_energy_mj, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+            
+            avg_dev_energy_mj+= dev_energy_mj;
+            avg_host_energy_mj+= host_energy_mj;
 
             float mem_cpy_t = std::chrono::duration_cast<std::chrono::microseconds>(mem_cpy_t_end - mem_cpy_t_start).count();
+            // Sum of all energy consumed by the host
             
             if (rank == 0) {
                 float mem_cpy_t_s = (mem_cpy_t * 2) / 1e+6;
@@ -113,13 +127,16 @@ int main(int argc, char *argv[]) {
                 float ar_time_s = (ar_time / 1e+6);
                 float single_run_time_s = (ar_time_s / chain_size);
                 avg_time_s += single_run_time_s;
-                std::cout << "ar_cuda_nccl," << "run_" << run << "," << chain_size << "," << buff_size_byte[i] << "," << mem_cpy_t_s * 1000 << "," << single_run_time_s * 1000 << "," << (data_Gb / single_run_time_s) << std::endl;
+                csv_file << "ar_cuda_nccl," << "run_" << run << "," << chain_size << "," << buff_size_byte[i] << "," << mem_cpy_t_s * 1000 << "," << single_run_time_s * 1000 << "," << (data_Gb / single_run_time_s)<< ","<< dev_energy_mj << ","<< host_energy_mj << std::endl;
+            
             }
         }
         if (rank == 0) {
             float data_Gb = static_cast<double>(buff_size_byte[i]) / 1.25e+8;
             avg_time_s /= MAX_RUN;
-            std::cout << "ar_cuda_nccl,run_avg," << chain_size << "," << buff_size_byte[i] << ",N/A," << avg_time_s * 1000 << "," << (data_Gb / avg_time_s) << std::endl;
+            avg_dev_energy_mj/=MAX_RUN;
+            avg_host_energy_mj/=MAX_RUN;
+            csv_file << "ar_cuda_nccl,run_avg," << chain_size << "," << buff_size_byte[i] << ",N/A," << avg_time_s * 1000 << "," << (data_Gb / avg_time_s) << "," << avg_dev_energy_mj << ","<< avg_host_energy_mj << std::endl;
         }
     }
     cudaMemcpy(d_recvbuf, h_recvbuf, buff_size_byte[num_iters-1], cudaMemcpyHostToDevice);
